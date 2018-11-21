ecb4854bc0d130d047d3f9e6a1455117188abe79
Frederick Barros <Frederick.Barros@br.ey.com>
2018-11-21 19:17:33 -0200

    teste de alteração arquivo principal

--- src/main/java/br/com/gsat/big/gerenciademenus/MsGerenciaDeMenusApplication.java
+++ src/main/java/br/com/gsat/big/gerenciademenus/MsGerenciaDeMenusApplication.java
@@ -8,5 +8,6 @@ public class MsGerenciaDeMenusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsGerenciaDeMenusApplication.class, args);
+		System.out.println("TEste Gerencia de MEnus");
 	}
 }
